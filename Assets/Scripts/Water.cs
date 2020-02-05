a8d15b80ad0858827a35085dd2fa3427b358a967
Tarcísio Costa <tarcisiotm@gmail.com>
2020-02-05 19:14:23 -0300

    refactored water code

--- Assets/Scripts/Water.cs
+++ Assets/Scripts/Water.cs
@@ -26,6 +26,7 @@ public class Water : MonoBehaviour
     public float fillAmount;
     public Dor[] dores;
     float auxDiff;
+    float over;
 
     int maxSameFrameLoop = 10;
     int loopCount = 0;
@@ -66,79 +67,84 @@ public class Water : MonoBehaviour
         {
             for (int i = 0; i < dores.Length; i++)
             {
-                float over = 0;
+                over = 0;
+
                 switch (dores[i].direction)
                 {
-                    case Dor.Side.DOWN:
-                        if (dores[i].door.isOpen && dores[i].water.fillAmount < 100)
-                        {
-                            //over = (amount != 0 ? amount : fillAmount > 0 ? fillAmount * 0.2f * Time.deltaTime: amount) / 2;
-                            over = 0;
-                            if (dores[i].water.fillAmount <= 95)
-                                over = fillAmount * .1f;
-                            dores[i].water.FillWater(over);
-
-                            fillAmount -= over;
-                            dores[i].water.FillWater(over);
-                        }
-                        break;
-                    case Dor.Side.RIGHT:
-                        if (dores[i].door.isOpen && dores[i].water.fillAmount < fillAmount)
-                        {
-                            auxDiff = (fillAmount - dores[i].water.fillAmount);
-
-                            over = auxDiff * .25f;
-                            if (dores[i].water.fillAmount + over > fillAmount - over &&
-                                dores[i].water.fillAmount - over > fillAmount + over
-                            )
-                            {
-                                Debug.Log("Possibly prevented stack overflow on right side.");
-                                //Debug.Break();
-                                over = (dores[i].water.fillAmount + over) / 2f;
-                            }
-
-                            fillAmount -= over;
-                            dores[i].water.FillWater(over);
-                        }
-                        break;
-                    case Dor.Side.LEFT:
-                        if (dores[i].door.isOpen && dores[i].water.fillAmount < fillAmount)
-                        {
-                            auxDiff = (fillAmount - dores[i].water.fillAmount);
-                            over = auxDiff * .25f;
-
-                            if (dores[i].water.fillAmount + over > fillAmount - over &&
-                                dores[i].water.fillAmount - over > fillAmount + over
-                                )
-                            {
-                                Debug.Log("Possibly prevented stack overflow on left side.");
-                                over = (dores[i].water.fillAmount + over) / 2f;
-                                //Debug.Break();
-                            }
-
-                            fillAmount -= over;
-                            dores[i].water.FillWater(over);
-                        }
-                        break;
-                    case Dor.Side.UP:
-                        if (dores[i].door.isOpen && fillAmount > 100)
-                        {
-                            if (dores[i].direction == Dor.Side.UP && dores[i].water.fillAmount < 95)
-                            {
-                                over = (fillAmount - 100);
-                                fillAmount -= over;
-                                dores[i].water.FillWater(over);
-                            }
-                        }
-                        break;
+                    case Dor.Side.DOWN: HandleDoorDown(dores[i]); break;
+                    case Dor.Side.RIGHT: HandleDoorRight(dores[i]); break;
+                    case Dor.Side.LEFT: HandleDoorLeft(dores[i]); break;
+                    case Dor.Side.UP: HandleDoorUp(dores[i]); break;
                 }
 
                 if (endGame && fillAmount >= 100 && !goingIntoMenu) {
                     goingIntoMenu = true;
                     StartCoroutine(FindObjectOfType<Player>().GoToMenu());
                 }
+
                 fillAmount = Mathf.Clamp(fillAmount, -1, 101);
             }
         }
+
+        void HandleDoorDown(Dor door) {
+            if (door.door.isOpen && door.water.fillAmount < 100) {
+                //over = (amount != 0 ? amount : fillAmount > 0 ? fillAmount * 0.2f * Time.deltaTime: amount) / 2;
+                over = 0;
+                if (door.water.fillAmount <= 95)
+                    over = fillAmount * .1f;
+                door.water.FillWater(over);
+
+                fillAmount -= over;
+                door.water.FillWater(over);
+            }
+        }
+
+        void HandleDoorRight(Dor door) {
+            if (door.door.isOpen && door.water.fillAmount < fillAmount) {
+                auxDiff = (fillAmount - door.water.fillAmount);
+
+                over = auxDiff * .25f;
+                if (door.water.fillAmount + over > fillAmount - over &&
+                    door.water.fillAmount - over > fillAmount + over
+                ) {
+                    Debug.Log("Possibly prevented stack overflow on right side.");
+                    //Debug.Break();
+                    over = (door.water.fillAmount + over) / 2f;
+                }
+
+                fillAmount -= over;
+                door.water.FillWater(over);
+            }
+        }
+
+        void HandleDoorLeft(Dor door) {
+            if (door.door.isOpen && door.water.fillAmount < fillAmount) {
+                auxDiff = (fillAmount - door.water.fillAmount);
+                over = auxDiff * .25f;
+
+                if (door.water.fillAmount + over > fillAmount - over &&
+                    door.water.fillAmount - over > fillAmount + over
+                    ) {
+                    Debug.Log("Possibly prevented stack overflow on left side.");
+                    over = (door.water.fillAmount + over) / 2f;
+                    //Debug.Break();
+                }
+
+                fillAmount -= over;
+                door.water.FillWater(over);
+            }
+        }
+
+        void HandleDoorUp(Dor door) {
+            if (door.door.isOpen && fillAmount > 100) {
+                if (door.direction == Dor.Side.UP && door.water.fillAmount < 95) {
+                    over = (fillAmount - 100);
+                    fillAmount -= over;
+                    door.water.FillWater(over);
+                }
+            }
+        }
+
+
     }
 }
