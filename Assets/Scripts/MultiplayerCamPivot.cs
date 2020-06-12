f0451decf220681bd77a8ec36835be035bab433e
felipesflisboa <felipesflisboa@users.noreply.github.com>
2020-06-12 14:54:25 -0300

    chore: removed unused code

--- Assets/Scripts/MultiplayerCamPivot.cs
+++ Assets/Scripts/MultiplayerCamPivot.cs
@@ -14,9 +14,6 @@ public class MultiplayerCamPivot : MonoBehaviour {
 
     Vector3 Position {
         get {
-            Vector3 ret = Vector3.zero;
-            //foreach (var player in playerArray) //remove
-            //    ret += player.transform.position; //remove
             return new Vector3(
                 (playerArray.Max((p) => p.transform.position.x) + playerArray.Min((p) => p.transform.position.x)) / 2,
                 (playerArray.Max((p) => p.transform.position.y) + playerArray.Min((p) => p.transform.position.y)) / 2,
@@ -45,7 +42,6 @@ public class MultiplayerCamPivot : MonoBehaviour {
             cam.transform.position = new Vector3(
                 cam.transform.position.x, cam.transform.position.y, zCameraPerMaxPlayerDistanceRange.Evaluate(GetMaxXDistanceBetweenPlayers())
             );
-            //maxDistance = GetMaxXDistanceBetweenPlayers(); //remove
             yield return null;
         }
     }
